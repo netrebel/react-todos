b346ede7e5cbb3bffe950dcdc23f36582ccfa9d1
Miguel Reyes <mreyes@life360.com>
2019-06-28 12:24:35 -0700

    Binding a button to a function

--- src/App.js
+++ src/App.js
@@ -8,14 +8,27 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todoItems : todosData.map(item => <TodoItem key={item.id} item={item} />)
+      todos: todosData,
+      count: 0
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
   }
 
   render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
     return (
       <div className="todo-list">
-        {this.state.todoItems}
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Click!</button>
+        {todoItems}
       </div>
     );
   }
