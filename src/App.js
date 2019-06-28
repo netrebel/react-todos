8387cc4bb426ccc2fb33cca1db5616a45c1b7cfa
Miguel Reyes <mreyes@life360.com>
2019-06-28 11:50:29 -0700

    Removed header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
@@ -9,10 +8,7 @@ function App() {
   const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className="todo-list">
       {todoItems}
     </div>
   );
